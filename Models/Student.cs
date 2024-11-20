b9bfe9d7003aa8b66dd61f6ada34339f17b7c0dd
oksana.myronovych <Oksana.Myronovych@ndsu.edu>
2024-11-20 13:44:55 -0600

    new Class Student

---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Myr_Nov20.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? StudentFirstName { get; set; }
+        public string? StudentLastName { get; set; }
+    }
+}
